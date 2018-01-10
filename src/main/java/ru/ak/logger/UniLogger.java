e6338199f21bc1e827f9dece5febaaa7d3c96581
Alexander Kakushin <alexandrkakushin@yandex.ru>
2018-01-11 00:52:49 +0300

    Инициализация проекта

---
+++ src/main/java/ru/ak/logger/UniLogger.java
@@ -0,0 +1,13 @@
+package ru.ak.logger;
+
+import javax.jws.WebService;
+
+
+/**
+ * Web-сервис для реализации интерфейса взаимодействия с SFTP-сервером
+ * @author akakushin
+ */
+@WebService(name = "Logger", serviceName = "Logger", portName = "LoggerPort")
+public class UniLogger {
+
+}
