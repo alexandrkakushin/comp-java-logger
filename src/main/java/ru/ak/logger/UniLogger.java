e498dc0b6ee8394321f96ecf6541decf8034103b
Alexander Kakushin <alexandrkakushin@yandex.ru>
2020-08-25 20:47:42 +0300

    issue#5

--- src/main/java/ru/ak/logger/UniLogger.java
+++ src/main/java/ru/ak/logger/UniLogger.java
@@ -1,16 +1,23 @@
 package ru.ak.logger;
 
-import ru.ak.logger.db.*;
+import ru.ak.logger.db.DbUtility;
+import ru.ak.logger.db.LoggerDataSource;
 import ru.ak.logger.db.connection.DbConnection;
 import ru.ak.logger.db.connection.SqliteConnection;
 import ru.ak.logger.db.dao.LevelController;
 import ru.ak.logger.db.dao.MessageController;
 import ru.ak.logger.db.dao.ObjectLogController;
-import ru.ak.model.*;
+import ru.ak.model.Level;
+import ru.ak.model.Message;
+import ru.ak.model.ObjectLog;
+import ru.ak.model.Response;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.Date;
@@ -20,16 +27,28 @@ import java.util.stream.Collectors;
 
 /**
  * Web-сервис для реализации логгера
- * 
+ *
  * @author a.kakushin
  */
 @WebService
 public class UniLogger {
 
-    private HashMap<DbConnection, LoggerDataSource> gates = new HashMap<>();
+    private final HashMap<DbConnection, LoggerDataSource> gates = new HashMap<>();
 
-    private LoggerDataSource getDataSource(DbConnection dbConnection) {
-        return this.gates.computeIfAbsent(dbConnection, key -> new LoggerDataSource(dbConnection));
+    private LoggerDataSource prepareDataSource(final DbConnection dbConnection) throws SQLException {
+
+        if (dbConnection instanceof SqliteConnection) {
+            final Path db = Paths.get(((SqliteConnection) dbConnection).getFileName());
+            if (!Files.exists(db)) {
+                this.gates.remove(dbConnection);
+            }
+        }
+
+        LoggerDataSource lds = this.gates.computeIfAbsent(dbConnection, key -> new LoggerDataSource(dbConnection));
+
+        DbUtility.init(lds);
+
+        return lds;
     }
 
     // Интерфейс взаимодействия
@@ -41,28 +60,28 @@ public class UniLogger {
     }
 
     @WebMethod(operationName = "messagesByPeriod")
-    public List<Message> messagesByPeriod(@WebParam(name = "connection") SqliteConnection connection,
-            @WebParam(name = "from") Date from, @WebParam(name = "to") Date to
+    public List<Message> messagesByPeriod(@WebParam(name = "connection") final SqliteConnection connection,
+            @WebParam(name = "from") final Date from, @WebParam(name = "to") final Date to
 
     ) throws SQLException, ParseException {
 
-        LoggerDataSource loggerDataSource = getDataSource(connection);
+        final LoggerDataSource loggerDataSource = prepareDataSource(connection);
 
-        MessageController messageDao = new MessageController(loggerDataSource);
+        final MessageController messageDao = new MessageController(loggerDataSource);
         return (List<Message>) messageDao.findByPeriodBetween(from, to);
     }
 
     @WebMethod(operationName = "clearMessages")
-    public Response clearMessages(@WebParam(name = "connection") SqliteConnection connection) {
-        Response response = new Response();
-        LoggerDataSource loggerDataSource = getDataSource(connection);
+    public Response clearMessages(@WebParam(name = "connection") final SqliteConnection connection) {
+        final Response response = new Response();
 
         try {
-            MessageController messageDao = new MessageController(loggerDataSource);
+            final LoggerDataSource loggerDataSource = prepareDataSource(connection);
+            final MessageController messageDao = new MessageController(loggerDataSource);
             messageDao.deleteAll();
 
             response.setObject("Success");
-        } catch (SQLException ex) {
+        } catch (final SQLException ex) {
             response.setError(true);
             response.setDescription(ex.getLocalizedMessage());
         }
@@ -71,18 +90,17 @@ public class UniLogger {
     }
 
     @WebMethod(operationName = "logSqlite")
-    public Response logSqlite(@WebParam(name = "connection") SqliteConnection connection,
-            @WebParam(name = "object") String object, @WebParam(name = "text") String text,
-            @WebParam(name = "level") String level) {
+    public Response logSqlite(@WebParam(name = "connection") final SqliteConnection connection,
+            @WebParam(name = "object") final String object, @WebParam(name = "text") final String text,
+            @WebParam(name = "level") final String level) {
 
-        Response response = new Response();
+        final Response response = new Response();
         try {
-            LoggerDataSource loggerDataSource = getDataSource(connection);
-            DbUtility.init(loggerDataSource);
+            final LoggerDataSource loggerDataSource = prepareDataSource(connection);
 
-            LevelController levelDao = new LevelController(loggerDataSource);
-            ObjectLogController objectLogDao = new ObjectLogController(loggerDataSource);
-            MessageController messageDao = new MessageController(loggerDataSource);
+            final LevelController levelDao = new LevelController(loggerDataSource);
+            final ObjectLogController objectLogDao = new ObjectLogController(loggerDataSource);
+            final MessageController messageDao = new MessageController(loggerDataSource);
 
             // Подготовка уровня
             Level levelDb = levelDao.findByName(level);
@@ -101,7 +119,7 @@ public class UniLogger {
             messageDao.create(new Message(objectLogDb, levelDb, text));
 
             response.setObject("Success");
-        } catch (Exception ex) {
+        } catch (final Exception ex) {
             response.setError(true);
             response.setDescription(ex.getLocalizedMessage());
         }
